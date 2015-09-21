91d562605ef24ddf1d4d1e3549cd88b92130f338
kels <chondrus@users.noreply.github.com>
2015-09-21 18:42:53 -0400

    clean up code, add some comments, add additional randomness

--- js/app.js
+++ js/app.js
@@ -1,43 +1,56 @@
 
 /*
- * Returns a random integer between min (included) and max (excluded)
- * Using Math.round() will give you a non-uniform distribution!
- * source:
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * Helper functions and globals
  */
+
+// there is probably a better way to do globals
+var startingRow = 5;
+var startingCol = 2;
+var baseWidth = 101;
+var baseHeight = 85;
+
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+// source:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+/*
+ * Classes and associated methods
+ */
+
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 
-    // location and speed are randomized;
+    // Location and speed are randomized;
     // games could vary wildly in difficulty.
-    // the bug's row (y) never changes, so store it for easier collision detection
+    // The bug's row (y) never changes, so it
+    // is just stored for easier collision detection.
     this.row = getRandomInt(1, 4);
-    this.x = (getRandomInt(-2, 5)) * 101;
-    this.y = (this.row * 85) - 25;
+    this.x = (getRandomInt(-2, 5)) * baseWidth;
+    this.y = (this.row * baseHeight) - 25;
     this.speed = getRandomInt(40, 400);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
+    // Multiply movement by the dt parameter - this ensures
+    // the game runs at the same speed for all computers.
     this.x += (this.speed *dt);
 
-    // wrap once it's well past the screen
-    if (this.x > 6*85){
-        this.x = -3*85;
+    // wrap once it's well past the screen to somewhere
+    // before the screen - the randomness here prevents
+    // a player from predicting when a bug will reappear
+    if (this.x > 5 * baseWidth) {
+        this.x = getRandomInt(-3 * baseWidth, -1 * baseWidth);
     }
 
     // collision detection
@@ -48,7 +61,7 @@ Enemy.prototype.update = function(dt) {
         // column collision detected modeled off of
         // https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
         // variables are only to enhance readability
-        var playerX = player.col * 101;
+        var playerX = player.col * baseWidth;
         var playerWidth = 90;
         var bugWidth = 85;
 
@@ -61,24 +74,26 @@ Enemy.prototype.update = function(dt) {
 };
 
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class - holds the information of the player character,
+// namely the image and the col and row location
 var Player = function() {
-    // variables applied to each instance
+
+    // currently, only one image available
     this.sprite = 'images/char-princess-girl.png';
 
     // player can only move in columns and rows, so that
     // is what is stored (as opposed to x and y)
-    this.col = 2;
-    this.row = 5;
+    this.col = startingCol;
+    this.row = startingRow;
 };
 
+// Player update: adds the new row and the col of the player
+// to the player's current row and col, and resets on win
 Player.prototype.update = function(col, row) {
     if (col !== undefined) {
         // col can only be [0-4]
@@ -121,17 +136,22 @@ Player.prototype.handleInput = function(input) {
     }
 };
 
+// returns the player to the beginning, in either win or collision
 Player.prototype.reset = function() {
-    this.col = 2;
-    this.row = 5;
+    this.col = startingCol;
+    this.row = startingRow;
 };
 
+// displays the player character
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.col * 101, this.row * 83);
+    ctx.drawImage(Resources.get(this.sprite), this.col * baseWidth, this.row * 83);
 };
 
 
-// Now instantiate your objects.
+/*
+ * Object Instantiation and Listener adding
+ */
+
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [
